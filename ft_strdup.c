148a936577e66a9fe52b8cfc9494f4927aef6505
Spanish Walrus <francisco-gutierr@hotmail.com>
2023-05-09 20:56:43 +0100

    calloc & strdup + notes

---
+++ ft_strdup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 11:38:55 by marvin            #+#    #+#             */
+/*   Updated: 2023/05/09 11:38:55 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*strdup() devuelve un puntero a una nueva cadena de caracteres que es un 
+duplicado de la cadena apuntada por s. La memoria para la nueva cadena se
+obtiene automáticamente con malloc(3), y puede (y debe) liberarse con free(3)*/
+
+char *strdup(const char *s)
+{
+    char  *newstr;
+    int    i;
+
+    i = 0;
+    newstr = (char *) malloc(sizeof (char) * ft_strlen(s) + 1);
+    if (newstr == '\0')
+        return (0);
+    while()
+
+    return (newstr)
+
+}
